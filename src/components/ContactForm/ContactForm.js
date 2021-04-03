71c55ea7a1f37f99bba8504164726570f7ea2a70
RomanStepanyan <stepik10@gmail.com>
2021-04-03 22:18:19 +0300

    Validation is added

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -44,6 +44,9 @@ export default class ContactForm extends Component {
         <input
           type="text"
           name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
           placeholder="Enter name"
           value={name}
           onChange={this.handleChangeForm}
@@ -52,6 +55,9 @@ export default class ContactForm extends Component {
         <input
           type="tel"
           name="phone"
+          pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+          title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+          required
           placeholder="Enter phone number"
           value={phone}
           onChange={this.handleChangeForm}
